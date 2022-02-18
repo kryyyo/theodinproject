c4d07f0e43bb6995cdf6080b2ebdad933938263c
Collene De Silva <collene.desilva@gmail.com>
2022-02-18 16:34:07 +0800

    Add answers and corrections for variable naming takss

---
+++ TheOdinProject/Javascript Basics/fundamentals-1/script.js
@@ -0,0 +1,17 @@
+// Tasks (practices)
+
+//Working with Variables - same solution
+let name, admin
+
+name = 'John'
+admin = name
+
+alert(admin)
+
+//Giving the Right Name 
+
+const ourPlanet = 'Earth';          //let ourPlanetName = "Earth";
+let currentVisitor = 'John';        //let currentUserName = "John";
+
+//note: a name with 3 words is fine
+
