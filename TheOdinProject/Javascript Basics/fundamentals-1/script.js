016597068cc60a0e3ee3616b687a597a509f5dd5
Collene De Silva <collene.desilva@gmail.com>
2022-02-19 03:02:29 +0800

    Add activity for fundamentals part 1

--- TheOdinProject/Javascript Basics/fundamentals-1/script.js
+++ TheOdinProject/Javascript Basics/fundamentals-1/script.js
@@ -1,17 +1,64 @@
-// Tasks (practices)
+// // Tasks (practices)
 
-//Working with Variables - same solution
-let name, admin
+// //Working with Variables - same solution
+// let name, admin
 
-name = 'John'
-admin = name
+// name = 'John'
+// admin = name
 
-alert(admin)
+// alert(admin)
 
-//Giving the Right Name 
+// //Giving the Right Name 
 
-const ourPlanet = 'Earth';          //let ourPlanetName = "Earth";
-let currentVisitor = 'John';        //let currentUserName = "John";
+// const ourPlanet = 'Earth';          //let ourPlanetName = "Earth";
+// let currentVisitor = 'John';        //let currentUserName = "John";
 
-//note: a name with 3 words is fine
+// //note: a name with 3 words is fine
 
+
+
+
+//Assignment (try the following exercises)
+
+// Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+    console.log(23+97)
+
+// Add a sequence of 6 different numbers together.
+    console.log(1+2+3+4+5+6)
+
+// Print the solution to the following equation: (4 + 6 + 9) / 77
+    let x = (4 + 6 + 9) / 77
+    console.log(x.toFixed(5))
+// Answer should be approximately 0.24675
+
+
+// Let’s use variables!
+// Type the following at the top of the script tag: let a = 10
+// In the console console.log(a) should print 10
+    let a = 10
+    console.log(a)
+
+// Try the following: 9 * a
+    9 * a
+
+// and this: let b = 7 * a (returns undefined *) and then console.log(b)
+    let b = 7 * a
+    console.log(b)
+
+// You should be getting the hang of this by now… try this sequence:
+// Declare a constant variable max with the value 57
+    const max = 57;
+
+// Set another variable actual to max - 13
+    const actual = max - 13;
+
+// Set another variable percentage to actual / max
+    const percentage = actual / max
+
+// If you type percentage in the console and press enter you should see a value like 0.7719
+    console.log(percentage.toFixed(4))
+
+
+
+
+// Take a few minutes to keep playing around with various things in your script tag. Eventually, we will learn how to actually make those numbers and things show up on the webpage, but all of this logic will remain the same, so make sure you’re comfortable with it before moving on.
