d067e45bb6d3afdccab0bbf10be5b9666f13e223
Collene De Silva <collene.desilva@gmail.com>
2022-02-20 20:12:24 +0800

    Add validation function for testing a RegEx Pattern

--- TheOdinProject/Javascript Basics/fundamentals-2/regex-playlist-lesson-10/validation.js
+++ TheOdinProject/Javascript Basics/fundamentals-2/regex-playlist-lesson-10/validation.js
@@ -1,5 +1,30 @@
 // validation script here
 
+//Testing a RegEx Pattern
+//Getting inputs from our index.html
+const inputs = document.querySelectorAll('input')
+
+
+//Telephone RegEx
 const patterns = {
     telephone: /^\d{11}$/
 }
+
+//validation function 
+function validate(field, regex){
+    // console.log(regex.test(field.value))
+
+    if(regex.test(field.value)) {
+        field.className = 'valid'
+    } else
+        field.className = 'invalid'
+}
+
+//Making JS Listen if you are typing on the input boxes
+inputs.forEach((input) => {
+    input.addEventListener('keyup',(e) => {
+        // console.log(e.target.attributes.name.value)
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
+
